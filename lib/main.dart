a0093fc20dbe3a4e64ceddfa02341f890cd66cf3
Hanna Alemayehu <hannaalemayehu2005@gmail.com>
2026-05-10 03:50:22 +0300

    new dependancies

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:carli_et/features/company/presentation/company_profile.dart';
-
+import 'package:carli_et/features/student/presentation/internship_details_page.dart';
+import 'package:carli_et/features/student/presentation/student_home_page.dart';
+import 'package:carli_et/features/student/presentation/browse_internships_page.dart';
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My First App',
+      title: 'CarLi_Et',
       debugShowCheckedModeBanner: false,
-      home: CompanyProfile(),
+      theme: ThemeData(
+        primaryColor: const Color(0xFF087E8B),
+        useMaterial3: true,
+      ),
+      
+      home: const StudentHomePage(), 
     );
   }
 }
